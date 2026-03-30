742a12e93e4d0d34edb5b3fb5d3a0f477fcd4879
Ryosuke Suzuki <ryosu0105@gmail.com>
2026-03-31 03:42:26 +0900

    Create main React component for anime management

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Anime Management</h1>
+            <p>Manage your anime collection efficiently!</p>
+        </div>
+    );
+}
+
+export default App;
